972594317039973f83a5703343e031e7a43c549b
SYNENTEC\RDsa <r.dsa@synentec.com>
2022-11-21 13:22:22 +0100

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+using NUnit.Framework;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            var deviceIdXml = new DeviceIdBuilder()
+                .OnWindows(windows => windows
+                    .AddMotherboardSerialNumber()
+                    .AddMacAddressFromWmi(excludeWireless: true, excludeNonPhysical: true))
+                    .UseFormatter(new XmlDeviceIdFormatter(new PlainTextDeviceIdComponentEncoder()))
+                    .ToString();
+            Console.WriteLine(deviceIdXml);
+        }
+    }
+}
